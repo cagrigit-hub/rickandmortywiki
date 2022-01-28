51dd689201ced02bcaa7ee1213b8aaad1566316f
cagrigit-hub <77447064+cagrigit-hub@users.noreply.github.com>
2022-01-28 07:02:18 +0300

    Add files via upload

--- pages/character/[id]/index.jsx
+++ pages/character/[id]/index.jsx
@@ -23,11 +23,15 @@ export default function Character({data,query}) {
     return random;
   }
   return (
-    <div className={styles.container}>
+    <div className={styles.container + " flex"}>
       <Head>
         <title>{name}</title>
-        
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Creepster&display=swap" rel="stylesheet"/>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
 
       <main className={styles.main}>
--- pages/index.js
+++ pages/index.js
@@ -106,6 +106,7 @@ export default function Home({data}) {
     <div className={styles.container + " flex"}>
       <Head>
         <title>Wubba Lubba Dub dub!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
